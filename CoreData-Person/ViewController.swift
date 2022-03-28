9901e35466718ec246735af321bb129442feaa5d
Gino Tasis <gtasis84@gmail.com>
2022-03-28 11:25:47 -0400

    Initial Commit

---
+++ CoreData-Person/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-Person
+//
+//  Created by Gino Tasis on 3/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
